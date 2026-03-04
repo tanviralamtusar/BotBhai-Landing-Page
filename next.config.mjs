3288136165ec56fa376a5ed140eb0fae6051edee
tanviralamtusar <tanviralamtusar@gmail.com>
2026-03-04 17:44:31 +0600

    update GitHub Actions workflow to use actions/configure-pages@v5 and enhance debug output

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
+  basePath: process.env.GITHUB_PAGES && process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
+    : '',
   typescript: {
     ignoreBuildErrors: true,
   },
